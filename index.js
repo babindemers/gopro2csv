900b42011276898e21365788b1c68748e31aaf71
babindemers <babw2501@usherbrooke.ca>
2020-06-23 11:14:30 -0400

    Changed path

--- index.js
+++ index.js
@@ -2,16 +2,18 @@ const gpmfExtract = require('gpmf-extract');
 const goproTelemetry = require('gopro-telemetry');
 const fs = require('fs');
 
-const matlab_path = 'C:/Users/willi/OneDrive/Documents/MATLAB/Stage/'
 
-const file = fs.readFileSync(matlab_path + '/video/20.MP4');
+const matlab_path = 'C:/Users/willi/OneDrive/Bureau/Stage/gopro_tug/';
+const video_name = 'TUG2';
+const video_type = '.MP4';
+const file = fs.readFileSync(matlab_path + 'video/' + video_name + video_type);
 
 gpmfExtract(file)
   .then(extracted => {
     let telemetry = goproTelemetry(extracted, {stream: ['ACCL', 'GYRO', 'CORI'], preset: 'csv', timeOut: 'cts'});
-    fs.writeFileSync(matlab_path + 'data/accl.csv', telemetry['HERO8 Black-ACCL']);
-    fs.writeFileSync(matlab_path + 'data/gyro.csv', telemetry['HERO8 Black-GYRO']);
-    fs.writeFileSync(matlab_path + 'data/cori.csv', telemetry['HERO8 Black-CORI']);
+    fs.writeFileSync(matlab_path + 'data/' + video_name + '_accl.csv', telemetry['HERO8 Black-ACCL']);
+    fs.writeFileSync(matlab_path + 'data/'  + video_name + '_gyro.csv', telemetry['HERO8 Black-GYRO']);
+    fs.writeFileSync(matlab_path + 'data/'  + video_name +  '_cori.csv', telemetry['HERO8 Black-CORI']);
     console.log('Telemetry saved as CSV');
   })
   .catch(error => console.log(error));
